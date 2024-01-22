fdd9f30bd37250430784c3a1360290d6eba53660
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-01-22 08:17:05 +0000

    Created database

--- resourcehub/routes.py
+++ resourcehub/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, url_for, request, redirect
 from resourcehub import app, db
-from werkzeug.security import generate_password_jash, check_password_hash
-# from resourcehub.models import User, Resource, Comment, Subject, EducationLevel
+from werkzeug.security import generate_password_hash, check_password_hash
+from resourcehub.models import User, Resource, Comment, Subject, EducationLevel
 
 
 @app.route("/")
@@ -34,7 +34,6 @@ def signup():
             new_user = User(username=username, email=email, password=generate_password_hash(password, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
-
             return redirect(url_for("home"))
 
     return render_template("signup.html")
