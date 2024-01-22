a92abb184c3abf140c2aa8e53ad127da7ff3d31b
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-01-22 00:11:37 +0000

    Created route for signup and edited models

--- resourcehub/models.py
+++ resourcehub/models.py
@@ -1,12 +1,15 @@
 from resourcehub import db
+from flask_login import UserMixin
+from datetime import datetime
+from enum import Enum
 
 
-class User(db.Model):
-    # schema for the User model
+class User(db.Model, UserMixin):
+    """ Schema for the User Model """
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     resources = db.relationship("Resource", backref="user", cascade="all, delete", lazy=True)
     comments = db.relationship("Comment", backref="user", cascade="all, delete", lazy=True)
 
@@ -15,38 +18,42 @@ class User(db.Model):
 
 
 class Resource(db.Model):
+    """ Schema for the Resource Model """
     id = db.Column(db.Integer, primary_key=True)
     resource_name = db.Column(db.String(50), unique=True, nullable=False)
     resource_description = db.Column(db.Text, nullable=False)
     url = db.Column(db.String(250), nullable=False)
     date_created = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
-    comments = db.relationship("Comment", backref="user", cascade="all, delete", lazy=True)
+    comments = db.relationship("Comment", backref="resource", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        return self 
+        return f"#{self.id}, name: {self.resource_name}"
 
 
 class Comment(db.Model):
+    """ Schema for the Comment Model """
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
+    comment_text = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     resource_id = db.Column(db.Integer, db.ForeignKey("resource.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return self.text
+        return self.comment_text
 
 
 class Subject(db.Model):
+    """ Schema for the Subject Model """
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    subject_name = db.Column(db.String(50), unique=True, nullable=False)
 
     def __repr__(self):
-        return self.name
+        return self.subject_name
 
 
 class EducationLevelEnum(Enum):
+    """ Enum for the Education levels """
     KS0 = "Early Years"
     KS1 = "Primary KS1"
     KS2 = "Primary KS2"
@@ -57,6 +64,7 @@ class EducationLevelEnum(Enum):
 
 
 class EducationLevel(db.Model):
+    """ Schema for the Education Level Model """
     id = db.Column(db.Integer, primary_key=True)
     level = db.Column(db.Enum(EducationLevelEnum), nullable=False)
 
@@ -64,21 +72,21 @@ class EducationLevel(db.Model):
         return self.level
 
 
-# Resource and Subject many-to-many relationship
 class ResourceSubject(db.Model):
+    """ Schema for the Resource and Subject many-to-many relationship """
     resource_id = db.Column(db.Integer, db.ForeignKey("resource.id"), primary_key=True)
     subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"), primary_key=True)
 
 
-# Resource and AgeRange many-to-many relationship
-class ResourceAgeRange(db.Model):
+class ResourceEducationLevel(db.Model):
+    """ Schema for the Resource and EducationLevel many-to-many relationship """
     id = db.Column(db.Integer, primary_key=True)
     resource_id = db.Column(db.Integer, db.ForeignKey("resource.id"))
-    age_range_id = db.Column(db.Integer, db.ForeignKey("age_range.id"))
+    education_level_id = db.Column(db.Integer, db.ForeignKey("education_level.id"))
 
 
-# Subject and AgeRange many-to-many relationship
-class SubjectAgeRange(db.Model):
+class SubjectEducationLevel(db.Model):
+    """ Schema for the Subject and EducationLevel many-to-many relationship """
     id = db.Column(db.Integer, primary_key=True)
     subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"))
-    age_range_id = db.Column(db.Integer, db.ForeignKey("age_range.id"))
+    education_level_id = db.Column(db.Integer, db.ForeignKey("education_level.id"))
--- resourcehub/routes.py
+++ resourcehub/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, url_for, request, redirect
 from resourcehub import app, db
+from werkzeug.security import generate_password_jash, check_password_hash
 # from resourcehub.models import User, Resource, Comment, Subject, EducationLevel
 
 
@@ -10,7 +11,6 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    
     return render_template("login.html")
 
 
@@ -21,4 +21,20 @@ def logout():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method =="POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirm-password")
+
+        # Validity Check
+        if password != confirmPassword: 
+            pass
+        else:
+            new_user = User(username=username, email=email, password=generate_password_hash(password, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for("home"))
+
     return render_template("signup.html")
