2885e683551b8a4fa908a6b650166b6be777d418
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-01-28 22:46:39 +0000

    Added route to view resource on new page

--- resourcehub/routes.py
+++ resourcehub/routes.py
@@ -9,84 +9,9 @@ from datetime import datetime
 from io import BytesIO
 
 
-
 @app.route("/")
 def home():
     return render_template("index.html", user=current_user)
-
-
-@app.route('/profile')
-@login_required
-def profile():
-    resources = list(Resource.query.order_by(Resource.date_created).all())
-    return render_template("profile.html", user=current_user, username=current_user.username, resources=resources)
-
-
-@app.route("/add_resource", methods=["GET", "POST"])
-def add_resource():
-    # Define subjects and education_levels for form
-    subjects = Subject.query.all()
-    education_levels = EducationLevel.query.all()
-
-    if request.method == "POST":
-        resource_title = request.form.get("resource_title")
-        resource_description = request.form.get("resource_description")
-        subject_name = request.form.get("subject")
-        education_level_name = request.form.get("education_level")
-
-        # Convert subject and education level names to IDs
-        subject_id = next((subject.id for subject in subjects if subject.subject_name == subject_name), None)
-        education_level_id = next((level.id for level in education_levels if level.level == education_level_name), None)
-        
-        if subject_id is None or education_level_id is None:
-            flash("Invalid subject or education level", category="error")
-            return redirect(url_for("add_resource"))
-
-        if "file" in request.files:
-            file = request.files["file"]
-            filename = secure_filename(file.filename)
-            data = file.read()
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-                
-        else:
-            flash("Invalid file", category="error")
-            return redirect(url_for("add_resource"))
-        
-        new_resource = Resource(
-            resource_title=resource_title,
-            resource_description=resource_description,
-            user_id=current_user.id,
-            file=filename,
-            data=data,
-            subject_id=subject_id,
-            education_level_id=education_level_id
-        )
-        db.session.add(new_resource)
-
-        db.session.commit()
-        flash("Resource added successfully", category="success")
-        return redirect(url_for("profile"))
-    return render_template("add_resource.html", user=current_user, username=current_user.username, subjects=subjects, education_levels=education_levels)
-
-@app.route("/download/<resource_id>")
-def download(resource_id):
-    resource = Resource.query.get_or_404(resource_id)
-
-    if resource:
-        # Retrieve only the file name
-        file_name = os.path.basename(resource.file)
-
-        # Create a BytesIO stream from the file data
-        file_data = BytesIO(resource.data)
-
-        return send_file(file_data, download_name=file_name, as_attachment=True)
-
-    # Handle the case where the resource with the given ID is not found
-    flash("Resource not found", category="error")
-    return redirect(url_for("profile"))
-
-    # upload = Resource.query.filter_by(id=resource_id).first()
-    # return send_file(BytesIO(upload.data), download_name=upload.file, as_attachment=True)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -139,3 +64,90 @@ def signup():
             return redirect(url_for("profile"))
 
     return render_template("signup.html", user=current_user)
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    # 
+    resources = list(Resource.query.order_by(Resource.date_created).all())
+    return render_template("profile.html", user=current_user, username=current_user.username, resources=resources)
+
+
+@app.route("/add_resource", methods=["GET", "POST"])
+@login_required
+def add_resource():
+    # Define subjects and education_levels for form
+    subjects = Subject.query.all()
+    education_levels = EducationLevel.query.all()
+
+    if request.method == "POST":
+        resource_title = request.form.get("resource_title")
+        resource_description = request.form.get("resource_description")
+        subject_name = request.form.get("subject")
+        education_level_name = request.form.get("education_level")
+
+        # Convert subject and education level names to IDs
+        subject_id = next((subject.id for subject in subjects if subject.subject_name == subject_name), None)
+        education_level_id = next((level.id for level in education_levels if level.level == education_level_name), None)
+        
+        if subject_id is None or education_level_id is None:
+            flash("Invalid subject or education level", category="error")
+            return redirect(url_for("add_resource"))
+
+        if "file" in request.files:
+            file = request.files["file"]
+            filename = secure_filename(file.filename)
+            data = file.read()
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+                
+        else:
+            flash("Invalid file", category="error")
+            return redirect(url_for("add_resource"))
+        
+        new_resource = Resource(
+            resource_title=resource_title,
+            resource_description=resource_description,
+            user_id=current_user.id,
+            file=filename,
+            data=data,
+            subject_id=subject_id,
+            education_level_id=education_level_id
+        )
+        db.session.add(new_resource)
+
+        db.session.commit()
+        flash("Resource added successfully", category="success")
+        return redirect(url_for("profile"))
+    return render_template("add_resource.html", user=current_user, username=current_user.username, subjects=subjects, education_levels=education_levels)
+
+
+@app.route("/download/<resource_id>")
+@login_required
+def download(resource_id):
+    resource = Resource.query.get_or_404(resource_id)
+
+    if resource:
+        # Retrieve only the file name
+        file_name = os.path.basename(resource.file)
+
+        # Create a BytesIO stream from the file data
+        file_data = BytesIO(resource.data)
+
+        return send_file(file_data, download_name=file_name, as_attachment=True)
+
+    # Handle the case where the resource with the given ID is not found
+    flash("Resource not found", category="error")
+
+    return redirect(url_for("profile"))
+
+
+@app.route("/view<int:resource_id>")
+def view(resource_id):
+    resource = Resource.query.get_or_404(resource_id)
+
+    if resource:
+        return render_template("view.html", user=current_user, resource=resource)
+    else:
+        flash("Resource not found", category="error")
+        return redirect(url_for("profile"))
