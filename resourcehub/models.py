b2e836804058ef9b9d4fdcbd5a6635b10c7a1740
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-02-06 14:14:50 +0000

    Made models.py pep8 compliant

--- resourcehub/models.py
+++ resourcehub/models.py
@@ -10,8 +10,18 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
-    resources = db.relationship("Resource", backref="user", cascade="all, delete", lazy=True)
-    comments = db.relationship("Comment", backref="user", cascade="all, delete", lazy=True)
+    resources = db.relationship(
+        "Resource",
+        backref="user",
+        cascade="all,
+        delete",
+        lazy=True)
+    comments = db.relationship(
+        "Comment",
+        backref="user",
+        cascade="all,
+        delete",
+        lazy=True)
 
     def __repr__(self):
         return f"#{self.id}, Username: {self.username}, Email: {self.email}"
@@ -24,11 +34,24 @@ class Resource(db.Model):
     resource_description = db.Column(db.Text, nullable=False)
     file = db.Column(db.String(250), nullable=False)
     data = db.Column(db.LargeBinary)
-    date_created = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    date_created = db.Column(
+        db.DateTime(),
+        default=datetime.utcnow,
+        nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"))
-    education_level_id = db.Column(db.Integer, db.ForeignKey("education_level.id"))
-    comments = db.relationship("Comment", backref="resource", cascade="all, delete", lazy=True)
+    education_level_id = db.Column(
+        db.Integer,
+        db.ForeignKey("education_level.id"))
+    comments = db.relationship(
+        "Comment",
+        backref="resource",
+        cascade="all,
+        delete",
+        lazy=True)
 
     def __repr__(self):
         return f"#{self.id}, title: {self.resource_title}"
@@ -38,9 +61,18 @@ class Comment(db.Model):
     """ Schema for the Comment Model """
     id = db.Column(db.Integer, primary_key=True)
     comment_text = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
-    resource_id = db.Column(db.Integer, db.ForeignKey("resource.id", ondelete="CASCADE"), nullable=False)
+    date_posted = db.Column(
+        db.DateTime(),
+        default=datetime.utcnow,
+        nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False)
+    resource_id = db.Column(
+        db.Integer,
+        db.ForeignKey("resource.id", ondelete="CASCADE"),
+        nullable=False)
 
     def __repr__(self):
         return self.comment_text
@@ -50,7 +82,11 @@ class Subject(db.Model):
     """ Schema for the Subject Model """
     id = db.Column(db.Integer, primary_key=True)
     subject_name = db.Column(db.String(120), unique=True, nullable=False)
-    resources = db.relationship("Resource", backref="subject", cascade="all, delete", lazy=True)
+    resources = db.relationship(
+        "Resource",
+        backref="subject",
+        cascade="all, delete",
+        lazy=True)
 
     def __repr__(self):
         return self.subject_name
@@ -60,9 +96,11 @@ class EducationLevel(db.Model):
     """ Schema for the Education Level Model """
     id = db.Column(db.Integer, primary_key=True)
     level = db.Column(db.String(120), unique=True, nullable=False)
-    resources = db.relationship("Resource", backref="education_level", cascade="all, delete", lazy=True)
+    resources = db.relationship(
+        "Resource",
+        backref="education_level",
+        cascade="all, delete",
+        lazy=True)
 
     def __repr__(self):
         return self.level
-
-
